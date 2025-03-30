f520aef25e13108dcc56707189af71ff9e797dfe
René Lacher <renemlacher@gmail.com>
2025-03-30 13:06:18 +0200

    feat: Added Main.runAlgorithms() method.
    - Factoring out the sorting algorithm execution in a separate method.
    - Creating Javadoc comments.

--- src/main/java/com/github/rlacher/sortanalyser/Main.java
+++ src/main/java/com/github/rlacher/sortanalyser/Main.java
@@ -3,6 +3,7 @@ package com.github.rlacher.sortanalyser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import com.github.rlacher.sortanalyser.core.SortAlgorithm;
 import com.github.rlacher.sortanalyser.dummy.DummySorter;
@@ -12,13 +13,15 @@ import com.github.rlacher.sortanalyser.dummy.DummySorter;
  * 
  * This class runs the sorting algorithms.
  */
-public class Main {
-
+public class Main
+{
 	// Initial test data: An unsorted array of integers.
 	private static final int[] TEST_DATA = {3, 7, 0, 9, 2, 5, 8, 1, 6, 4};
 
 	/**
      * The main method, which starts the sorting algorithm benchmarking process.
+     * 
+     * This class initializes and runs a list of sorting algorithms against a predefined test dataset.
      *
      * @param args Command-line arguments (currently unused).
      */
@@ -27,10 +30,21 @@ public class Main {
         List<SortAlgorithm> sortAlgorithms = new ArrayList<>();
         sortAlgorithms.add(new DummySorter());
         
-        for(SortAlgorithm sorter : sortAlgorithms)
+        runAlgorithms(sortAlgorithms, TEST_DATA);
+    }
+
+    /**
+     * Runs the provided sorting algorithms on the given test data.
+     * 
+     * @param algorithms The list of sorting algorithms to be executed.
+     * @param testData The test data to be sorted. A copy of the data is created for each algorithm.
+     */
+    private static void runAlgorithms(List<SortAlgorithm> algorithms, final int[] testData)
+    {
+        for (SortAlgorithm algorithm : algorithms)
         {
-			int[] testDataCopy = Arrays.copyOf(TEST_DATA, TEST_DATA.length);
-			sorter.sort(testDataCopy);
-		}
+            int[] dataCopy = Arrays.copyOf(testData, testData.length);
+            algorithm.sort(dataCopy);
+        }
     }
 }
